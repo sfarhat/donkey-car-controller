6891362b9b4029a6f55e59ad2423f4a643c3d9e2
Sean Farhat <seanfarhat9@gmail.com>
2018-08-03 16:27:03 -0700

    uploaded demo

--- trajectory.py
+++ trajectory.py
@@ -3,7 +3,7 @@ import numpy as np
 
 while 1:
 	for x in np.arange(989):
-		filename = "frame_" + str(x) + ".png"
+		filename = "./images/frame_" + str(x) + ".png"
 		img = cv2.imread(filename)
 		cv2.imshow("Car POV", img)
 		k = cv2.waitKey(1) #### ALWAYS NEED WAITKEY WHEN DOING IMSHOW
